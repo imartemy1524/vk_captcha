38b2b0b741144e7aaad34f169120670f021eadca
imartemy1524 <41122604+imartemy1524@users.noreply.github.com>
2022-06-24 14:37:21 +0300

    Delete vk_api_handler.py

--- vk_api_handler.py
+++
@@ -1,6 +0,0 @@
-import vk_api
-from .solver import VkCaptchaSolver
-Solver = VkCaptchaSolver()
-class VkApiCaptcha(vk_api.VkApi):
-    def captcha_handler(self, captcha):
-        return Solver.vk_api_captcha_handler(captcha)
